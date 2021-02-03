cc64704a2f7420df0f284b4c157d9f7770dd2028
kylieanyce <kylieanyce@gmail.com>
2021-02-03 13:08:17 -0600

    added new api key

--- scripts/.Settings.js
+++ scripts/.Settings.js
@@ -1,5 +1,5 @@
 export const settings = {
 	graphhopperKey: "b9444e6b-2fec-424b-8c2e-4ae00f87c3f3",
-	npsKey: "zflqCsoWMQi4mVagiujDzfWb1TZZxTAAbkviJ1Hk",
+	npsKey: "iS64Pl03HwTtlj5MKPdCon0gBhpKACS3fsxtq0kv",
 	weatherKey: "6fa8865ca2fdcb9f09e8011d5d4a517c"
 };
