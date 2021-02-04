ad78fe534d5af39dc2a194d0c38fb4ac9963306b
kylieanyce <kylieanyce@gmail.com>
2021-02-04 15:48:39 -0600

    worked on weatherlist

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -1,14 +1,25 @@
 import { useWeather, getWeather} from "./WeatherProvider.js"
 
-
+const contentTarget = document.querySelector(".weatherPreview")
 
 
 export const WeatherList = () => {
     getWeather()
         .then(() => {
             const weather = useWeather()
-            console.log(weather)
+            // console.log(weather)
             render(weather)
         })
+}
+
+const render = weatherCollection => {
+debugger
+console.log(weatherCollection)
+    contentTarget.innerHTML = `
+        <section class="weatherContainer value="${weatherCollection[0].id}">
+            <div class="weatherContainer--items">${weatherCollection[0].main}</div>
+            <div class="weatherContainer--items">${weatherCollection[0].description}</div>
+            <div class="weatherContainer--items">${weatherCollection[0].icon}</div>
+    `
 }
 
--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,12 +1,17 @@
 let weather = [];
 
 
-export const useWeather = () => weather.weather.slice();
+export const useWeather = () => weather.slice();
 
 
 //get weather from API
 export const getWeather = () => {
     return fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3f8451e0b46939dd52d03da8c2011d89")
         .then(response => response.json())
-        .then(weatherArray => weather = weatherArray)
+        .then(
+            parsedWeather => {
+                weather = parsedWeather
+            }
+        )
+        .then(weatherArray => weather = weatherArray.weather)
 }
