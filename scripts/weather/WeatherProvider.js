78a90066ebb965ffdd007dee73839ec12d5d0c0f
kylieanyce <kylieanyce@gmail.com>
2021-02-04 21:55:56 -0600

    worked on weather provider

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -1,17 +1,16 @@
-let weather = [];
+let weather = {};
 
 
-export const useWeather = () => weather.slice();
+export const useWeather = () => {
+    ...weather
+};
 
 
 //get weather from API
 export const getWeather = () => {
     return fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3f8451e0b46939dd52d03da8c2011d89")
         .then(response => response.json())
-        .then(
-            parsedWeather => {
-                weather = parsedWeather
-            }
-        )
-        .then(weatherArray => weather = weatherArray.weather)
+        .then(parsedWeather => weather = parsedWeather)
+        .then()
 }
+
