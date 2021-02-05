6abf121068c52ea4e2f7dcbda1a3711310cb9921
kylieanyce <kylieanyce@gmail.com>
2021-02-05 13:54:44 -0600

    moved api keys from public file to git ignore file

--- scripts/.Settings.js
+++ scripts/.Settings.js
@@ -1,5 +1,6 @@
 export const settings = {
-	graphhopperKey: "b9444e6b-2fec-424b-8c2e-4ae00f87c3f3",
-	npsKey: "iS64Pl03HwTtlj5MKPdCon0gBhpKACS3fsxtq0kv",
-	weatherKey: "6fa8865ca2fdcb9f09e8011d5d4a517c"
+	graphhopperKey: "",
+	npsKey: "",
+	weatherKey: ""
 };
+
--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -8,7 +8,7 @@ export const useWeather = () => {
 
 //get weather from API
 export const getWeather = () => {
-    return fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=3f8451e0b46939dd52d03da8c2011d89")
+    return fetch("http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=")
         .then(response => response.json())
         .then(parsedWeather => weather = parsedWeather)
         .then()
