ff69cbf48d7034a931bc63ff34f1ec36de50602a
Caleb James <calebsjames@gmail.com>
2021-02-05 18:32:16 -0600

    bizararrarrary fix

--- scripts/itineraries/Itineraries.js
+++ scripts/itineraries/Itineraries.js
@@ -6,7 +6,7 @@ export const itineraryHTMLConverter = (itineraryObject) => {
             
             <div class="itineraryPark"><b>Park:</b> ${itineraryObject.parkName}</div>
             <div class="itineraryEatery"><b>Eatery:</b> ${itineraryObject.eateryName}</div>
-            <div class="itineraryBizarraries"><b>Bizarrary:</b> ${itineraryObject.bizarrieName}</div>
+            <div class="itineraryBizarraries"><b>Bizarrary:</b> ${itineraryObject.bizarraryName}</div>
         </section>
     `
 }
