423e97aece4a01aaa143da66619d247636be98c9
kylieanyce <kylieanyce@gmail.com>
2021-02-08 09:40:56 -0600

    switched API calls

--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -18,10 +18,10 @@ const weatherAPIkey = settings.weatherKey
 
 //get weather from API-----------------------------------------------------------
 export const getWeather = () => {
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=37.5858662&lon=-85.67330523&appid=${weatherAPIkey}`)
+    return fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=37.5858662&lon=-85.67330523&exclude=minutely,hourly,alerts&appid=d5b900ebf7bb2e5d1d38789cbb0736b9&units=imperial`)
         .then(response => response.json())
         .then(parsedWeather => {
-            weather = parsedWeather.list
+            weather = parsedWeather.current.temp
             // debugger
             // for (const aCity in parsedWeather.city){
             //     const cityID = parsedWeather.city["id"]
