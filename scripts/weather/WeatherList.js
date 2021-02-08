eae7a090f160d8848f45f621a751b34f5c7ae358
kylieanyce <kylieanyce@gmail.com>
2021-02-08 11:52:39 -0600

    cleaned up code and added guide notes for team

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -2,7 +2,7 @@ import { getWeather, useWeather } from "./WeatherProvider.js"
 
 const contentTarget = document.querySelector(".weatherPreview")
 
-
+//gets weather data from API, puts into an array to be rendered----------
 export const WeatherList = () => {
     getWeather()
         .then(() => {
@@ -11,35 +11,31 @@ export const WeatherList = () => {
         })
 }
 
+//renders 5 day forcast to dom---------------------------------------------
 const render = weatherCollection => {
-    // debugger
-// console.log(weatherCollection)
     contentTarget.innerHTML = `
         <section class="weatherContainer">
             <div class="dayOne>
                 <div class="dayOneTemp>${weatherCollection[0]}</div>
                 <div class="dayOneDescription">${weatherCollection[5]}</div>
             </div>
-            <div class="dayOne>
-                <div class="dayOneTemp>${weatherCollection[1]}</div>
-                <div class="dayOneDescription">${weatherCollection[6]}</div>
+            <div class="dayTwo>
+                <div class="dayTwoTemp>${weatherCollection[1]}</div>
+                <div class="dayTwoDescription">${weatherCollection[6]}</div>
             </div>
-            <div class="dayOne>
-                <div class="dayOneTemp>${weatherCollection[2]}</div>
-                <div class="dayOneDescription">${weatherCollection[7]}</div>
+            <div class="dayThree>
+                <div class="dayThreeTemp>${weatherCollection[2]}</div>
+                <div class="dayThreeDescription">${weatherCollection[7]}</div>
             </div>
-            <div class="dayOne>
-                <div class="dayOneTemp>${weatherCollection[3]}</div>
-                <div class="dayOneDescription">${weatherCollection[8]}</div>
+            <div class="dayFour>
+                <div class="dayFourTemp>${weatherCollection[3]}</div>
+                <div class="dayFourDescription">${weatherCollection[8]}</div>
             </div>
-            <div class="dayOne>
-                <div class="dayOneTemp>${weatherCollection[4]}</div>
-                <div class="dayOneDescription">${weatherCollection[9]}</div>
+            <div class="dayFive>
+                <div class="dayFiveTemp>${weatherCollection[4]}</div>
+                <div class="dayFiveDescription">${weatherCollection[9]}</div>
             </div>
         </section>
         `
     }
     
-    
-    // ${weatherCollection.map(weatherObj => {
-    //     `${weatherObj.temp}`}) }
