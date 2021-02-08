c87afd5b9d8ba51e8574c68834bbedf35aca1def
kylieanyce <kylieanyce@gmail.com>
2021-02-08 11:39:30 -0600

    temp and description are displayed on dom gs .. gaa

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -15,13 +15,31 @@ const render = weatherCollection => {
     // debugger
 // console.log(weatherCollection)
     contentTarget.innerHTML = `
-        <section class="weatherContainer value="${weatherCollection.map(weatherObj => {
-            `${weatherObj.temp}`}) }">
-        <div class="weatherContainer--items">${weatherCollection}</div>
-        <div class="weatherContainer--items">${weatherCollection}</div>
-        <div class="weatherContainer--items">${weatherCollection}</div>
+        <section class="weatherContainer">
+            <div class="dayOne>
+                <div class="dayOneTemp>${weatherCollection[0]}</div>
+                <div class="dayOneDescription">${weatherCollection[5]}</div>
+            </div>
+            <div class="dayOne>
+                <div class="dayOneTemp>${weatherCollection[1]}</div>
+                <div class="dayOneDescription">${weatherCollection[6]}</div>
+            </div>
+            <div class="dayOne>
+                <div class="dayOneTemp>${weatherCollection[2]}</div>
+                <div class="dayOneDescription">${weatherCollection[7]}</div>
+            </div>
+            <div class="dayOne>
+                <div class="dayOneTemp>${weatherCollection[3]}</div>
+                <div class="dayOneDescription">${weatherCollection[8]}</div>
+            </div>
+            <div class="dayOne>
+                <div class="dayOneTemp>${weatherCollection[4]}</div>
+                <div class="dayOneDescription">${weatherCollection[9]}</div>
+            </div>
         </section>
         `
     }
     
     
+    // ${weatherCollection.map(weatherObj => {
+    //     `${weatherObj.temp}`}) }
--- scripts/weather/WeatherProvider.js
+++ scripts/weather/WeatherProvider.js
@@ -7,12 +7,22 @@ let day3 = []
 let day4 = []
 let day5 = []
 
+let temp1 = []
+let temp2 = []
+let temp3 = []
+let temp4 = []
+let temp5 = []
+
+let des1 = []
+let des2 = []
+let des3 = []
+let des4 = []
+let des5 = []
+
 export const useWeather = () => {
-    weather.push(day1, day2, day3, day4, day5)
-    console.log(weather)
+    weather.push(temp1, temp2, temp3, temp4, temp5, des1, des2, des3, des4, des5)
     return weather.slice()
 };
-
 
 const weatherAPIkey = settings.weatherKey
 // console.log(weatherAPIkey)
@@ -28,7 +38,17 @@ export const getWeather = () => {
             day4 = parsedWeather.daily[3]
             day5 = parsedWeather.daily[4]
             
-            console.log(day1, day2, day3, day4, day5)
+            temp1 = day1.temp.day
+            temp2 = day2.temp.day
+            temp3 = day3.temp.day
+            temp4 = day4.temp.day
+            temp5 = day5.temp.day
+
+            des1 = day1.weather[0].description
+            des2 = day2.weather[0].description
+            des3 = day3.weather[0].description
+            des4 = day4.weather[0].description
+            des5 = day5.weather[0].description
         })
         // .then(() => console.log(weather, city))
 }
