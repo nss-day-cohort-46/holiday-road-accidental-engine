b857735933a48dace427795a00b93917386feac5
Caleb James <calebsjames@gmail.com>
2021-02-08 11:34:08 -0600

    removed debugger

--- scripts/parks/parkSelect.js
+++ scripts/parks/parkSelect.js
@@ -12,7 +12,7 @@ const contentTarget = document.getElementById("parksButton")
 eventHub.addEventListener("change", changeEvent => {
     //if eventListener detects a change on the "parkSelect" element...
     if (changeEvent.target.id === "parkDropdown") {
-        debugger
+        
         //gets the name of the currently selected park
         const selectedPark = changeEvent.target.value
 
